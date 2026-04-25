436563613a0bebbbb6329e26e285e95fd908836d
jianan <Apple624233@>
2026-04-25 19:51:32 +0800

    初始化项目

---
+++ src/main/java/com/jianan/jiananpassagercreater/JiananPassagerCreaterApplication.java
@@ -0,0 +1,13 @@
+package com.jianan.jiananpassagercreater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiananPassagerCreaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiananPassagerCreaterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jianan/jiananpassagercreater/JiananPassagerCreaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jianan.jiananpassagercreater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiananPassagerCreaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
