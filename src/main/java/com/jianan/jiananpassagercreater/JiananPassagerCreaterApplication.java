533d3e26d94168296dbd1ec655271400e184bb11
jianan <Apple624233@>
2026-04-25 20:39:50 +0800

    用户管理

--- src/main/java/com/jianan/jiananpassagercreater/JiananPassagerCreaterApplication.java
+++ src/main/java/com/jianan/jiananpassagercreater/JiananPassagerCreaterApplication.java
@@ -1,8 +1,12 @@
 package com.jianan.jiananpassagercreater;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy(exposeProxy = true)
+@MapperScan("com.jianan.jiananpassagercreater.mapper")
 @SpringBootApplication
 public class JiananPassagerCreaterApplication {
 
---
+++ src/main/java/com/jianan/jiananpassagercreater/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.jianan.jiananpassagercreater.mapper;
+
+import com.jianan.jiananpassagercreater.model.entity.User;
+import com.mybatisflex.core.BaseMapper;
+
+/**
+ * 用户 映射层。
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
